263fc605b6230b1aa2a6245a5dee3a698c1a87fc
Julian Piasecki <julian.piasecki@outlook.com>
2024-05-16 00:51:04 +0200

    chore: upgrade to clerk v5

--- app/links/page.tsx
+++ app/links/page.tsx
@@ -1,6 +1,6 @@
 import LinksTable from '@/components/LinksTable';
 import { getUserLinks } from '@/lib/actions';
-import { currentUser } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
 const LinksPage = async () => {
