b63962af7eccdcf489af70b77a477e547fa5ab4a
Julian Piasecki <julian.piasecki@outlook.com>
2024-07-12 04:06:21 +0200

    fix: a11y focus classes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "bg-secondary text-primary hover:bg-secondary/80 focus-visible:ring-black focus-visible:ring-offset-primary",
+          "bg-secondary text-primary hover:bg-secondary/80 focus-visible:!ring-black focus-visible:!ring-offset-primary",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
